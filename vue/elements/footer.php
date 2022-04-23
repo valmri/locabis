4d02615b5d1647d48c594abf10fad96a04a79b89
marmie <valentin.marmie@outlook.com>
2022-04-23 02:06:22 +0200

    Ajout des fichiers js

--- vue/elements/footer.php
+++ vue/elements/footer.php
@@ -1,5 +1,7 @@
 <footer>
     
 </footer>
+<script src="./node_modules/sweetalert2/dist/sweetalert2.all.min.js"></script>
+<script src="./assets/js/ajax/annulationReservation.js"></script>
 </body>
 </html>
