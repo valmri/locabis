59345ae27ad32111dbcc246e0265791111fea945
valmri <valmarmie87@outlook.com>
2022-05-20 01:49:23 +0200

    Responsive design

--- vue/elements/head.php
+++ vue/elements/head.php
@@ -8,4 +8,5 @@
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <link rel="stylesheet" href="./node_modules/flatpickr/dist/flatpickr.min.css">
     <link rel="stylesheet" href="./assets/css/styles.css">
+    <link rel="stylesheet" href="./assets/css/responsive.css">
 </head>
--- vue/elements/header.php
+++ vue/elements/header.php
@@ -1,5 +1,5 @@
 <header>
-    <div class="menu">
+    <div class="menu menuDesk">
         <span class="logo">
             <a href="?page=accueil"></a>
         </span>
@@ -39,5 +39,49 @@
             </div>
             
         </nav>
+    </div>
+
+    <div class="menu menuResp">
+        <span class="logo">
+            <a href="?page=accueil"></a>
+        </span>
+
+        <nav class="navigation">
+
+            <div class="boutonNavigations">
+                <ul>
+                    <li>
+                        <a class="boutonMenu" href="?page=accueil">Accueil</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="boutonConnecte">
+                <ul>
+                    <?php if (!isset($_SESSION['utilisateur']) && !isset($_SESSION['jeton'])) :?>
+                        <li>
+                            <a class="boutonMenu" href="?page=inscription">Inscription</a>
+                        </li>
+                        <li>
+                            <a class="boutonMenu" href="?page=connexion">Connexion</a>
+                        </li>
+                    <?php else :?>
+                        <li>
+                            <a class="boutonMenu" href="?page=membre">Espace membre</a>
+                        </li>
+                        <?php if(isset($_SESSION['utilisateur']['role']) && (int)$_SESSION['utilisateur']['role'] === 2) : ?>
+                            <li>
+                                <a class="boutonMenu" href="?page=proprietaire">Espace proprietaire</a>
+                            </li>
+                        <?php endif; ?>
+                        <li>
+                            <a class="boutonMenu" href="?page=deconnexion">Déconnexion</a>
+                        </li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+
+        </nav>
+
     </div>
 </header>
